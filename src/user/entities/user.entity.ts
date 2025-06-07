40686d0cbee465a01ed275f954f12ed7849e8ed5
Joseph Oluchukwu <devjoseph016@gmail.com>
2025-06-07 14:00:06 +0100

    feat: ✨ Enhance UserService with user retrieval methods and improve User entity structure

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,10 +1,9 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Plan } from '../../plan/entities/plan.entity';
+import { Column, Entity, OneToMany } from 'typeorm';
+import { BaseEntity } from '../../shared/entities/base.entity';
 
 @Entity('users')
-export class User {
-  @PrimaryGeneratedColumn()
-  id: number;
-
+export class User extends BaseEntity {
   @Column({ type: 'varchar', length: 255, unique: true })
   email: string;
 
@@ -16,4 +15,7 @@ export class User {
 
   @Column({ type: 'int', default: 0 })
   wallet: number; // Saved in kobo
+
+  @OneToMany(() => Plan, (plan) => plan.user, { cascade: true })
+  plans: Plan[];
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
@@ -6,8 +6,7 @@ import { User } from './entities/user.entity';
 @Injectable()
 export class UserService {
   constructor(
-    @InjectRepository(User)
-    private userRepository: Repository<User>,
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
   ) {}
 
   async seed(): Promise<void> {
@@ -39,5 +38,25 @@ export class UserService {
     } else {
       console.log('Users already exist, skipping seed');
     }
+  }
+
+  async findUserByEmail(email: string) {
+    const user = await this.userRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
+  }
+
+  async findUserById(id: number) {
+    const user = await this.userRepository.findOne({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
   }
 }
